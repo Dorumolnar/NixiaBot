621db7007dbd6f245980d45e054f4543e309de9f
Dorumolnar <35419205+Dorumolnar@users.noreply.github.com>
2018-02-27 16:43:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,9 @@ client.on('message', message => {
     	message.reply('For PC: http://www.mediafire.com/file/z41c0vgndpcxpwf/NixiaGDPS+2.11.zip');
         message.reply('For android: http://www.mediafire.com/file/hfgfhgo5ac015z0/NixiaGDPS+2.11.apk');
   	}
+    if (message.content === 'nb!gdpstextures') {
+        message.reply('Texture for gdps: http://www.mediafire.com/file/psg48o4fg733z5q/NixiaGDPS+2.11+Resources.zip');
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
