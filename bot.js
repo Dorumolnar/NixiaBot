811815f85f284f1167bce4e5a077480f4a8bff0b
Dorumolnar <35419205+Dorumolnar@users.noreply.github.com>
2018-02-27 16:51:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,19 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
 client.on('message', message => {
@@ -12,6 +25,9 @@ client.on('message', message => {
   	}
     if (message.content === 'nb!gdpstextures') {
         message.reply('Texture for gdps: http://www.mediafire.com/file/psg48o4fg733z5q/NixiaGDPS+2.11+Resources.zip');
+    }
+    if (message.content === 'gpdsmodapp') {
+       message.reply('Mod app: https://goo.gl/forms/xGIJGTc3QNvbLQt72')   
     }
 });
 
